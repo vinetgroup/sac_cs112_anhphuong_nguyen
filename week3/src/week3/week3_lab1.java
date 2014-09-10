79e5d648d76464d4877ecbda61532225bce2c532
anhphuong nguyen <vinetgroup@yahoo.com>
2014-09-09 19:51:42 -0700

    Commit Lab1

---
+++ week3/src/week3/week3_lab1.java
@@ -0,0 +1,11 @@
+package week3;
+
+public class week3_lab1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("My name is Anhphuong Nguyen");
+
+	}
+
+}
