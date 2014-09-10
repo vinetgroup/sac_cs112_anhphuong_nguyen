d34c04b081de746ca7dbe61566b963f6f940c1d0
anhphuong nguyen <vinetgroup@yahoo.com>
2014-09-09 20:11:48 -0700

    Commit lab2

--- week3/src/week3/week3_lab2.java
+++ week3/src/week3/week3_lab2.java
@@ -3,7 +3,7 @@ package week3;
 public class week3_lab2 {
 
 	public static void main(String[] args) {
-		System.out.println(" Welcome to ");
+		System.out.println("Welcome to ");
 		System.out.println("Java Programing !");
 
 	}
