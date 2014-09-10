b49f8853d626adce2c8e2b5944287a402e15379a
anhphuong nguyen <vinetgroup@yahoo.com>
2014-09-09 21:45:06 -0700

    lab5

---
+++ week3/src/week3/lab5.java
@@ -0,0 +1,19 @@
+package week3;
+import java.util.Scanner;
+
+public class lab5 {
+
+	public static void main(String[] args) {
+	
+		Scanner input = new Scanner( System.in);
+	
+		int number1; // 1 number to  enter 
+		int number2; // 2 number to enter
+System.out.print(" Hello enter first number:");
+number1 = input.nextInt();
+
+System.out.print(" Hello EnterSecond number:");
+number2 = input.nextInt();
+
+	}
+}
