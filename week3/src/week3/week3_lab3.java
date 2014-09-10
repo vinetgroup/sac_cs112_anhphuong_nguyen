9dba4de7f8981a4c0c3dcf25e253af1d472776bf
anhphuong nguyen <vinetgroup@yahoo.com>
2014-09-09 20:55:00 -0700

    lab3

---
+++ week3/src/week3/week3_lab3.java
@@ -0,0 +1,12 @@
+package week3;
+
+public class week3_lab3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String num1 = "Hello";
+		String num2 = "World";
+		System.out.println(num1 + " " + num2);
+	}
+
+}
