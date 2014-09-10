eefd9e1e00e94b2c02d6f5f3a35c4601abceebeb
anhphuong nguyen <vinetgroup@yahoo.com>
2014-09-09 21:09:24 -0700

    commit lab4

--- week3/src/week3/lab4.java
+++ week3/src/week3/lab4.java
@@ -1,12 +1,11 @@
 package week3;
 
-public class week3_lab3 {
+public class lab4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String num1 = "Hello";
 		String num2 = "World";
-		System.out.printf(num1 + " \n" + num2);
+		System.out.printf(num1  +" \n" + num2);
 	}
 
 }
