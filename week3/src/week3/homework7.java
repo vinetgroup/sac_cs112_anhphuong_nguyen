5859abcd63a8d54afe4b042a5eb79aac94a11e29
anhphuong nguyen <vinetgroup@yahoo.com>
2014-09-16 14:00:34 -0700

    commit homework 7

---
+++ week3/src/week3/homework7.java
@@ -0,0 +1,35 @@
+package week3;
+
+public class homework7 {
+
+	public static void main(String[] args) {
+		int number1 = 100;
+		int number2 = 5;
+		int sum, different, mutiply,divison ;
+		
+		//perform arithmetic operation
+		sum = number1 + number2 ;
+		different = number1 - number2 ;
+		mutiply = number1 * number2 ;
+		divison = number1 / number2 ;
+		
+		// print result
+		System.out.println(" The sum, different, mutiply,divison of" );
+		System.out.println(number1);
+		System.out.println(" and ");
+		System.out.println(number2);
+		System.out.println("are");
+		System.out.println(sum);
+		System.out.println(",");
+		System.out.println(different);
+		System.out.println(",");
+		System.out.println(mutiply);
+		System.out.println(",");
+		System.out.println(divison);
+		
+		
+		
+
+	}
+
+}
