f9c8a1aa8501a32311e897e8b60c2f5bfcb05ab6
anhphuong nguyen <vinetgroup@yahoo.com>
2014-09-23 13:54:20 -0700

    week5 homework 15.5 A

---
+++ week3/src/week3/week515A.java
@@ -0,0 +1,19 @@
+package week3;
+
+public class week515A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for ( int i = 1; i <= 10; i++ ) 
+			// for ( int i = 10; i >= 1; i-- ) 
+			 {
+			 for ( int j = 1; j <= i; j++ )
+				 
+			System.out.print( '*' );
+
+			 System.out.println();
+			 } 
+
+	}
+
+}
